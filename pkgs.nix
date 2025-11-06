f4d25003b6450f9e0208a305e9f33ee2916bfe84
whoamitty <whoamitty.pro+gh@gmail.com>
2025-11-06 17:25:19 -0300

    better:organization:pkgs

--- pkgs.nix
+++ pkgs.nix
@@ -2,64 +2,56 @@
 
 {
 environment.systemPackages = with pkgs; [
-	#gui
+	#GUI
 	onlyoffice-desktopeditors vscode ghostty pairdrop thunderbirdPackages.thunderbird-latest
 	brave audio-recorder tenacity obsidian
 	windsurf blender gimp3 planify gnome-frog
         #pairdrop beeper 
-
-
-
+	inputs.zen-browser.packages."${system}".beta
+        shutter
 	
-	( inputs.zen-browser.packages."${system}".beta-unwrapped.override  {
-	policies = {
-   AutofillAddressEnabled = true;
-    AutofillCreditCardEnabled = false;
-    DisableAppUpdate = true;
-    DisableFeedbackCommands = true;
-    DisableFirefoxStudies = true;
-    DisablePocket = true;
-    DisableTelemetry = true;
-    DontCheckDefaultBrowser = true;
-    NoDefaultBookmarks = true;
-    OfferToSaveLogins = false;
-    EnableTrackingProtection = {
-      Value = true;
-      Locked = true;
-      Cryptomining = true;
-      Fingerprinting = true;
-    };
-  };
-} 
-)
+        #CLI
+	#It work alone
+        cron vdhcoappa
+        
+
+        git yt-dlp ripgrep 
+        shellcheck holehe flatpak jujutsu vhs radicle-node 
+        xan tldr notify-desktop zip unp md2pdf bat tree curl 
+        unzip nix-search fastfetch jq xclip httpie stow borgbackup
+        ffmpeg wget  
 	
-	#tui
-	vim zoxide yt-dlp ripgrep git mpv ffmpeg bat tmux nushell
-	gh yazi zellij fastfetch nix-search jq fzf curl nom tree 
-	borgbackup bc ripgrep-all stow see-cat glow xclip
-        cron wget unzip zip unp vdhcoapp httpie zsh fish jujutsu vhs radicle-node shutter
-	md2pdf lynx xan tldr notify-desktop  shellcheck holehe
+        #TUI (interactive)
+
+        #shell
+        zsh fish nushell
+
+        #nothing to edit
+        fzf glow see-cat ripgrep-all 
+       
+        #edit with
+        gh zoxide 
+        vim mpv tmux bc
+	yazi zellij nom lynx 
+	 
         #vhs shell video recorder
         #nh #rigep-all #ytfzf
 	#googler abandoned
 	#ventoy not secure
+ 
 
 	#plugin tmux
 	tmuxPlugins.resurrect	
 
 	#extensions gnome
-	gnomeExtensions.battery-health-charging
-	gnomeExtensions.boost-volume
+	# gnomeExtensions.battery-health-charging
+	# gnomeExtensions.boost-volume
 
 	#dev
 	sqlite
     	python311
 	bash-completion
 
-        # DON'T TOUCH ANYMORE
-        # php
-	# php84Packages.composer
-        
 
 	# Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   # wget
